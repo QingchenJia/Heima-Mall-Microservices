c1e41a172d5fa08fb9c22f9b4d14c1bf7cc70624
Qingchen_Jia <879484952@qq.com>
2025-01-15 15:30:39 +0800

    Service splitting: Create a new shopping cart service and configuration

---
+++ cart-service/src/main/java/com/hmall/cart/domain/po/Cart.java
@@ -0,0 +1,40 @@
+package com.hmall.cart.domain.po;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName("cart")
+public class Cart implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    private Long userId;
+
+    private Long itemId;
+
+    private Integer num;
+
+    private String name;
+
+    private String spec;
+
+    private Integer price;
+
+    private String image;
+
+    private LocalDateTime createTime;
+
+    private LocalDateTime updateTime;
+}
