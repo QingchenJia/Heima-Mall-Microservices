4b16d1aa0282d50850d241c4ec4016c7c5de79a7
Qingchen_Jia <879484952@qq.com>
2025-01-17 20:48:08 +0800

    Service splitting: Split the trade microservices and configurations

---
+++ trade-service/src/main/java/com/hmall/trade/mapper/OrderDetailMapper.java
@@ -0,0 +1,7 @@
+package com.hmall.trade.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hmall.trade.domain.po.OrderDetail;
+
+public interface OrderDetailMapper extends BaseMapper<OrderDetail> {
+}
---
+++ trade-service/src/main/java/com/hmall/trade/mapper/OrderMapper.java
@@ -0,0 +1,7 @@
+package com.hmall.trade.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hmall.trade.domain.po.Order;
+
+public interface OrderMapper extends BaseMapper<Order> {
+}
