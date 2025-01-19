f291f384ab264292454c309d2e7f9ef33baf436a
Qingchen_Jia <879484952@qq.com>
2025-01-19 14:29:53 +0800

    Gateway Configuration: Create a gateway module and configure routing information

---
+++ user-service/src/main/java/com/hmall/user/service/IAddressService.java
@@ -0,0 +1,7 @@
+package com.hmall.user.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmall.user.domain.po.Address;
+
+public interface IAddressService extends IService<Address> {
+}
